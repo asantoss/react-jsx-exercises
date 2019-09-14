fa849fe577636edfed48c3fba1650dba2dee1e65
Alexander Santos <alexsantosantana@live.com>
2019-09-14 11:00:35 -0400

    Created my first component

--- first-component/app.js
+++ first-component/app.js
@@ -1,5 +1,22 @@
+
+
 let FirstComponent = () => {
-    return <span>Make this component render like the Windows Logo: <a href="https://goo.gl/images/mY5Qdv" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+    const mainStyle = {
+        width: '400px',
+        display: 'flex',
+        flexWrap: '1'
+    }
+    return <div style={mainStyle}>
+        <div className="row">
+            <div style={{ backgroundColor: 'orange', }} className="square" ></div>
+            <div style={{ backgroundColor: 'blue' }} className="square"></div>
+        </div>
+        <div className="row">
+            <div style={{ backgroundColor: 'lime' }} className="square"></div>
+            <div style={{ backgroundColor: 'yellow' }} className="square"></div>
+        </div>
+
+    </div>
 }
 
 ReactDOM.render(<FirstComponent />, document.getElementById('root'))
